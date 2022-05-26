696ba3d9fc1bbd3943b24231871f4b7b5f315ff2
apius <notyoudev@gmail.com>
2022-05-26 00:05:38 -0400

    rss

--- hakyll/site.hs
+++ hakyll/site.hs
@@ -9,6 +9,16 @@ import Text.Pandoc.Options
 import Hakyll
 --------------------------------------------------------------------------------
 
+-- RSS & Atom
+feedConfig::FeedConfiguration
+feedConfig = FeedConfiguration { 
+  feedTitle       = "notes<u2on", 
+  feedDescription = "various notes and writings on various topics to various degrees of completeness and accuracy",
+  feedAuthorName  = "u2on",
+  feedAuthorEmail = "me@notu.dev",
+  feedRoot        = "https://notu.dev/notes"
+}
+
 -- so that it exports to the correct folder
 config::Configuration
 config = defaultConfiguration {
@@ -16,51 +26,68 @@ config = defaultConfiguration {
 }
 
 main::IO()
-main = hakyllWith config $ do
-    match ("images/*") $ do
-        route   idRoute
-        compile copyFileCompiler
+main = hakyllWith config $ do  
+  match ("images/*") $ do
+    route   idRoute
+    compile copyFileCompiler
 
-    match ("css/*.css") $ do
-        route   idRoute
-        compile compressCssCompiler
+  match ("css/*.css") $ do
+    route   idRoute
+    compile compressCssCompiler
   
-    -- tags    
-    tags <- buildTags "posts/*.md" (fromCapture ("tags/*.html"))
-    tagsRules tags $ \tag pattern -> do
-        let title = "items tagged |" ++ tag ++ "|"
-        route idRoute
-        compile $ do
-            items <- recentFirst =<< loadAll pattern
-            let ctx = constField "title" title
-                      `mappend` listField "items" (iCtxTags tags) (return items)
-                      `mappend` defaultContext
+  -- tags    
+  tags <- buildTags "posts/*.md" (fromCapture ("tags/*.html"))
+  tagsRules tags $ \tag pattern -> do
+      let title = "items tagged |" ++ tag ++ "|"
+      route idRoute
+      compile $ do
+        items <- recentFirst =<< loadAll pattern
+        let ctx = constField "title" title
+                  `mappend` listField "items" (iCtxTags tags) (return items)
+                  `mappend` defaultContext
 
-            makeItem ""
-                >>= loadAndApplyTemplate ("templates/tag.html") ctx
-                >>= loadAndApplyTemplate ("templates/default.html") ctx
-                >>= relativizeUrls
+        makeItem ""
+          >>= loadAndApplyTemplate ("templates/tag.html") ctx
+          >>= loadAndApplyTemplate ("templates/default.html") ctx
+          >>= relativizeUrls
 
-    match "posts/*.md" $ do
-        route $ setExtension "html"
-        compile $ pandocMathCompiler
-            >>= loadAndApplyTemplate ("templates/item.html")    (iCtxTags tags)
-            >>= loadAndApplyTemplate ("templates/default.html") (iCtxTags tags)
-            >>= relativizeUrls
+  match "posts/*.md" $ do
+    route $ setExtension "html"
+    compile $ pandocMathCompiler
+      >>= loadAndApplyTemplate ("templates/item.html")    (iCtxTags tags)
+      >>= saveSnapshot "content"
+      >>= loadAndApplyTemplate ("templates/default.html") (iCtxTags tags)
+      >>= relativizeUrls
 
-    match ("index.html") $ do
-        route idRoute
-        compile $ do
-            items <- recentFirst =<< loadAll "posts/*.md" 
-            let indexCtx =
-                    listField "items" iCtx (return items) `mappend`
-                    defaultContext
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+  match ("index.html") $ do
+    route idRoute
+    compile $ do
+      items <- recentFirst =<< loadAll "posts/*.md" 
+      let indexCtx =
+              listField "items" iCtx (return items) `mappend`
+              defaultContext
+      getResourceBody
+          >>= applyAsTemplate indexCtx
+          >>= loadAndApplyTemplate "templates/default.html" indexCtx
+          >>= relativizeUrls 
+  -- literally the same thing, just different names
+  create ["atom.xml"] $ do
+    route idRoute
+    compile $ do
+        let feedCtx = iCtx `mappend` bodyField "description"
+        posts <- fmap (take 10) . recentFirst =<<
+            loadAllSnapshots "posts/*" "content"
+        renderAtom feedConfig feedCtx posts
 
-    match "templates/*" $ compile templateBodyCompiler
+  create ["rss.xml"] $ do
+    route idRoute
+    compile $ do
+        let feedCtx = iCtx `mappend` bodyField "description"
+        posts <- fmap (take 10) . recentFirst =<<
+            loadAllSnapshots "posts/*" "content"
+        renderAtom feedConfig feedCtx posts
+
+  match "templates/*" $ compile templateBodyCompiler
 
 --------------------------------------------------------------------------------
 iCtx::Context String
