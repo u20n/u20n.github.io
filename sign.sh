d4fede873d9763c9b5c5ce4a2122e7d80f18fe78
apius <git@notu.dev>
2023-05-26 16:52:18 -0400

    RSS + txt --> html

--- sign.sh
+++ sign.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-rm docs/index.html.gpg
+
+# generate RSS
+cat $(ls -d docs/txt/*) | urss > docs/txt.xml
+
+rm -f docs/index.html.gpg
 gpg -s --default-key D44D3077F7AED1E0B7D7F7AED6301856767EE50E docs/index.html
