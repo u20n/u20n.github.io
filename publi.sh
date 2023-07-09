32f45224e822c2ffede240ffce7bb5738fd39231
apius <git@notu.dev>
2023-07-09 01:03:50 -0400

    slim

--- publi.sh
+++ publi.sh
@@ -3,8 +3,8 @@
 # generate RSS
 sh ./genxml.sh > docs/txt.xml
 
-for f in $(find ./docs/* -type f -not -name "*.gpg")
+for f in $(find ./docs/* -type f -not -name "*.asc")
 do
-  rm -f "$f.gpg"
-  gpg -s --default-key D44D3077F7AED1E0B7D7F7AED6301856767EE50E "$f"
+  rm -f "$f.asc"
+  gpg --clearsign --default-key D44D3077F7AED1E0B7D7F7AED6301856767EE50E "$f"
 done
