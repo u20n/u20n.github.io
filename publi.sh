a9e3296ef43dea0261a30c02e3445a1d6ff2a904
virtus <me@notu.dev>
2023-07-29 19:47:40 +0000

    add index, others

--- publi.sh
+++ publi.sh
@@ -3,8 +3,12 @@
 # generate RSS
 sh ./genxml.sh > docs/txt.xml
 
-for f in $(find ./docs/* -type f -name "*.txt")
-do
-  rm -f "$f.asc"
-  gpg --clearsign --default-key D44D3077F7AED1E0B7D7F7AED6301856767EE50E "$f"
-done
+# generate txt index
+cd docs; sdir.sh txt > txt_index.html; cd ..;
+
+
+#for f in $(find ./docs/* -type f -name "*.txt")
+#do
+  #rm -f "$f.asc"
+  #gpg --clearsign --default-key D44D3077F7AED1E0B7D7F7AED6301856767EE50E "$f"
+#done
